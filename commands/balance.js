bdae2d1817cd895d77c796582b4896d1175d0066
Tony Baker <tbaker@tdsi.com>
2021-02-26 22:19:04 -0500

    Added Balance reporting to DB when using the balance command.

--- commands/balance.js
+++ commands/balance.js
@@ -5,6 +5,8 @@ var minimist = require('minimist')
   , moment = require('moment')
   , exchangeService = require('../lib/services/exchange-service')
   , { formatCurrency } = require('../lib/format')
+  , collectionService = require('../lib/services/collection-service')
+  , tb = require('timebucket')
 
 module.exports = function (program, conf) {
   program
@@ -43,6 +45,14 @@ module.exports = function (program, conf) {
       so.debug = cmd.debug
       so.json = cmd.json
       so.mode = 'live'
+
+      // Add the Mongo Collection Service
+      var collectionServiceInstance = collectionService(conf)
+      var balancesCollection = collectionServiceInstance.getBalances()
+
+      function exit() {
+        process.exit();
+      }
       function balance() {
         var exchange = exchangeServiceInstance.getExchange()
 
@@ -56,32 +66,25 @@ module.exports = function (program, conf) {
           exchange.getQuote(s, function (err, quote) {
             if (err) throw err
 
+            // Get a Timebucket stamp
+            let now = Date();
+            var d = tb(now).resize(conf.balance_snapshot_period)
+
             // If JSON results are requested
             var json = {}
-            if (so.json) {
-              // Create a result object for JSON
-              var json = {
-                timestamp: moment().format('YYYY-MM-DD HH:mm:ss'),
-                asset: s.product_id.split('-')[0],
-                currency: s.product_id.split('-')[1],
-                asset_balance: balance.asset,
-                asset_hold: balance.asset_hold,
-                asset_available: n(balance.asset).subtract(balance.asset_hold).value(),
-                asset_value: n(balance.asset).multiply(quote.ask).value(),
-                currency_hold: balance.currency_hold,
-                currency_available: n(balance.currency).subtract(balance.currency_hold).value(),
-                total: n(balance.asset).multiply(quote.ask).add(balance.currency).value()
-              }
-            } else {
-
-              // Not JSON output, write the formatted data to the CLI
-              var bal = moment().format('YYYY-MM-DD HH:mm:ss').grey + ' ' + formatCurrency(quote.ask, s.currency, true, true, false) + ' ' + (s.product_id).grey + '\n'
-              bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Asset: '.grey + n(balance.asset).format('0.00000000').white + ' Available: '.grey + n(balance.asset).subtract(balance.asset_hold).value().toString().yellow + '\n'
-              bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Asset Value: '.grey + n(balance.asset).multiply(quote.ask).value().toString().white + '\n'
-              bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Currency: '.grey + n(balance.currency).format('0.00000000').white + ' Available: '.grey + n(balance.currency).subtract(balance.currency_hold).value().toString().yellow + '\n'
-              bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Total: '.grey + n(balance.asset).multiply(quote.ask).add(balance.currency).value().toString().white
-              console.log(bal)
+            // Create a result object for JSON
+            var json = {
+              id: so.selector.normalized + '-' + d.toString(),
+              selector: so.selector.normalized,
+              time: d.toMilliseconds(),
+              currency: balance.currency,
+              asset: balance.asset,
+              price: quote.ask,
+              consolidated: n(balance.asset).multiply(quote.ask).add(balance.currency).value()
             }
+
+            // Clone the ID to Mongo's preferred _id
+            json._id = json.id
 
             // If Calculating to another currency is requested
             if (so.calculate_currency) {
@@ -106,19 +109,30 @@ module.exports = function (program, conf) {
                 })
               })
             }
-            else {
 
-              // Return the JSON Data
-              if (so.json) {
-                console.log(JSON.stringify(json))
-              }
-
-              process.exit()
+            // Return the JSON Data
+            if (so.json) {
+              console.log(JSON.stringify(json))
+            } else {
+              // Not JSON output, write the formatted data to the CLI
+              var bal = moment().format('YYYY-MM-DD HH:mm:ss').grey + ' ' + formatCurrency(quote.ask, s.currency, true, true, false) + ' ' + (s.product_id).grey + '\n'
+              bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Asset: '.grey + n(balance.asset).format('0.00000000').white + ' Available: '.grey + n(balance.asset).subtract(balance.asset_hold).value().toString().yellow + '\n'
+              bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Asset Value: '.grey + n(balance.asset).multiply(quote.ask).value().toString().white + '\n'
+              bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Currency: '.grey + n(balance.currency).format('0.00000000').white + ' Available: '.grey + n(balance.currency).subtract(balance.currency_hold).value().toString().yellow + '\n'
+              bal += moment().format('YYYY-MM-DD HH:mm:ss').grey + ' Total: '.grey + n(balance.asset).multiply(quote.ask).add(balance.currency).value().toString().white
+              console.log(bal)
             }
+
+            // Add the Balance to the Database, replacing a previous record, if one exists.
+            balancesCollection.replaceOne({_id: json.id}, json, {upsert: true}, function (err, records) {
+              if(err) throw err;
+              process.exit();
+            });
           })
         })
       }
 
+      // Run the Get Balance
       balance()
     })
 }
